f58942a0426690cec2692910fa36867bb3abecc0
kpankov <Kirill.Pankov@bia-tech.ru>
2022-11-11 20:14:58 +0300

    01-MeetupTitle task solution

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,42 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+   name:'App',
+  data() {
+     return {
+       meetupsRange: [1,2,3,4,5],
+       meetups: [],
+       MeetUpTitle: 'MEETUP_TITLE',
+       meetupSelected: null,
+       meetupApiHeaders : []
+     }
+  },
+  watch: {
+    meetupApiHeaders: {
+      immediate: true,
+      handler() {
+        this.meetupApiHeaders = this.getMeetUpFromApi
+    }
+      },
+    meetupSelected: function() {
+       this.meetupApiHeaders[this.meetupSelected].then(value => {
+             this.MeetUpTitle = value.title
+       });
+    }
+
+  },
+  computed: {
+     getMeetUpFromApi() {
+       let meetUpsList = {};
+       for (let item of this.meetupsRange) {
+         let data = fetchMeetupById(item);
+         meetUpsList[item] = data;
+       }
+       return meetUpsList
+     }
+  }
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
